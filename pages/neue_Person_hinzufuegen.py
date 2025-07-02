1f6e0e0e39d6eea6a66452ae92057a18bd42227a
dh823 <dh8239@mci4me.at>
2025-07-02 17:37:47 +0200

    Neue Person anlegen funzt

--- pages/neue_Person_hinzufuegen.py
+++ pages/neue_Person_hinzufuegen.py
@@ -18,7 +18,7 @@ with tab1:
     # Formular zur Eingabe neuer Personendaten
     with st.form("person_form"):
         st.markdown("Pflichtfelder sind mit * markiert")
-        new_person_firstname = st.text_input("Vorname*", key="new_person_firstname").upper
+        new_person_firstname = st.text_input("Vorname*", key="new_person_firstname").upper()
         new_person_lastname = st.text_input("Nachname*", key="new_person_lastname").upper()
         new_person_date_of_birth = st.date_input("Geburtsdatum*", value=dt.date(2000, 1, 1), key="new_person_birthdate").year
         new_person_gender = st.selectbox("Geschlecht*", ["female", "male"], key="new_person_gender")
@@ -111,88 +111,6 @@ with tab1:
                         current_person_list.append(neue_person)
                         Person.safe_person_data(current_person_list)
                         st.success(f"{new_person_firstname} {new_person_lastname} wurde erfolgreich hinzugefügt!")
-
-
-
-
-
-
-
-
-
-                # if exists:
-                #     st.error(f"Die Person {new_person_firstname} {new_person_lastname} existiert bereits in der Datenbank.")
-                #     st.stop()
-                # else:
-                #     new_id = max([p["id"] for p in current_person_list], default=0) + 1
-
-                #     new_person_picture_path = ""
-                #     new_person_ekg_file_path = ""
-                #     new_person_ftg_file_path = ""
-
-                #     os.makedirs("data/pictures", exist_ok=True)
-                #     os.makedirs("data/ekg_files", exist_ok=True)
-
-                #     if new_person_picture:
-                #         picture_path = f"data/pictures/{new_id}_{new_person_picture_path.name}"
-                #         with open(picture_path, "wb") as f:
-                #             f.write(new_person_picture.getbuffer())
-                            
-                #     if new_person_ekg_file:
-                #         ekg_file_path = f"data/ruhe_ekg_{new_id}_{new_person_ekg_file.name}"
-                #         with open(ekg_file_path, "wb") as f:
-                #             f.write(new_person_ekg_file.getbuffer())
-                    
-                #     if new_person_ftp_file:
-                #         ftg_file_path = f"data/ftp_tests/{new_id}_{new_person_ftp_file.name}"
-                #         with open(ftg_file_path, "wb") as f:
-                #             f.write(new_person_ftp_file.getbuffer())
-
-
-                #     ekg_tests = []
-                #     os.makedirs(ekg_folder, exist_ok=True)
-
-                #     # Lade bestehende EKG-IDs aus der Datenbank, um fortlaufende IDs zu erzeugen
-                #     all_ekg_ids = [ekg["id"] for person in current_person_list for ekg in person.get("ekg_tests", [])]
-                #     next_ekg_id = max(all_ekg_ids, default=0) + 1
-
-                #     # Falls du mehrere EKGs zulassen willst
-                #     uploaded_ekg_files = [new_person_ekg_file] if new_person_ekg_file else []
-
-                #     for ekg_file in uploaded_ekg_files:
-                #         if ekg_file:
-                #             file_name = f"{new_id}_{ekg_file.name}"
-                #             save_path = os.path.join(ekg_folder, file_name)
-                #             with open(save_path, "wb") as f:
-                #                 f.write(ekg_file.getbuffer())
-
-                #             ekg_tests.append({
-                #                 "id": next_ekg_id,
-                #                 "date": dt.date.today().isoformat(),  # Oder beliebiges Datum
-                #                 "result_link": save_path
-                #             })
-                #             next_ekg_id += 1
-
-
-                #     neue_person = {
-                #         "id": new_id,
-                #         "firstname": new_person_firstname.strip(),
-                #         "lastname": new_person_lastname.strip(),
-                #         "date_of_birth": int(new_person_date_of_birth),
-                #         "picture_path": new_person_picture_path,
-                #         "gender": new_person_gender,
-                #         for test in 
-                #             "ekg_tests": [{
-                #                 "id": int(ekg_id),
-                #                 "date": ekg_date,
-                #                 "result_link": ekg_result_link
-                #             }]
-                #     }
-
-                #     current_person_list.append(neue_person)
-                #     Person.safe_person_data(current_person_list)
-
-                #     st.success(f"{new_person_firstname} {new_person_lastname} wurde erfolgreich hinzugefügt!")
 
 with tab2:
     st.title("Bestehende Personen bearbeiten")
